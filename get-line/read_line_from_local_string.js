677630adcafe80d866db8cf780a48e3398673a2d
leo zacharry <leo.zacharry@gmail.com>
2018-08-31 02:33:47 +0300

    Examples

--- get-line/read_line_from_local_string.js
+++ get-line/read_line_from_local_string.js
@@ -1,16 +1,14 @@
   require('get-data-line');
 
-  var myvar = `Hello_line_1\nHello_line_2\nHello_line_3`;
-
-  loadFile("./demo.txt");
+  var myvar = `Hello_line_1\nHello_line_2\nHello_line_3\nHello_line_4`;
 
   //READ ONLY LINES -> 5,6,7,8,9,10
-  String(myvar).getline([5,6,7,8,9,10], function(data){
+  String(myvar).getline([1,3,4], function(data){
     console.log( data )
   });
 
   //REMOVE LINES -> 1,3,4,6
-  String(myvar).remline ([1,3,4,6],function(data){
+  String(myvar).remline ([3,4],function(data){
     console.log( data )
   });
 
---
+++ get-line/read_line_from_txt_.js
@@ -0,0 +1,30 @@
+  require('get-data-line');
+
+  var myvar = ``;
+
+  loadFile("./demo.txt");
+
+  //READ ONLY LINES -> 5,6,7,8,9,10
+  String(myvar).getline([5,6,7,8,9,10], function(data){
+    console.log( data )
+  });
+
+  //REMOVE LINES -> 1,3,4,6
+  String(myvar).remline ([1,3,4,6],function(data){
+    console.log( data )
+  });
+
+
+  /*
+  String(myvar).remline ([1,3],function(data){
+    console.log( data )
+  });
+
+  String(myvar).getline([1,2], function(data){
+    console.log( data )
+  });
+
+  String(myvar).read( function(data){
+    console.log( data )
+  });
+ */
